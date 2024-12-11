478d737a0b1f4363167d74cacf5b28cfc1d5cf0e
doom-2 <phramov945@gmail.com>
2024-12-12 00:28:32 +0300

    Initial commit Chapter3 / Lesson_2

---
+++ Chapter_3/Lesson_2/Task_1/task_1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+static int count = 0;
+
+auto fib(unsigned int n)
+{
+    count++;
+    std::cout << "count = " << count << std::endl;
+    if (n <= 1)
+        return n;
+    return fib(n - 1) + fib(n - 2);
+}
+
+
+int main()
+{
+    std::cout << fib(10) << std::endl;
+    return 0;
+}
---
+++ Chapter_3/Lesson_2/Task_2/task_2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+auto fib(unsigned int n)
+{
+    size_t f[100];
+    int i;
+    f[0] = 0;
+    f[1] = 1;
+    for (int i = 2; i <= n; i++) {
+        f[i] = f[i - 1] + f[i - 2];
+    }
+    return f[n];
+}
+
+int main()
+{
+    std::cout << fib(3) << std::endl;
+    return 0;
+}
